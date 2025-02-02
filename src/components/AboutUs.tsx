f49ae34ff641df34a60e38c9bb8663f1734458d3
M-o-ha-med <120997421+M-o-ha-med@users.noreply.github.com>
2025-02-02 18:08:55 +0700

    Update AboutUs.tsx

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -15,7 +15,7 @@ return (
       <div className="md:col-span-2">
         <div className="max-w-lg md:max-w-none">
 		  <h3 className="text-3xl text-justify mt-4 text-gray-700 font-bold" >Mengubah Properti Impian Anda Menjadi Kenyataan.</h3>
-          <p className="text-2xl text-justify mt-4 text-gray-700">
+          <p className="text-xl xl: text-2xl md:text-2xl text-justify mt-4 text-gray-700">
 			Dengan pengalaman lebih dari 7 tahun, telah sukses mengerjakan proyek arsitektur mulai dari rumah pribadi hingga kompleks perumahan berskala besar,baik di dalam maupun luar negeri.
 			Kami berdedikasi untuk membantu Anda membangun properti impian yang tidak hanya fungsional dan indah, tetapi juga melebihi ekspektasi Anda.
           </p>
