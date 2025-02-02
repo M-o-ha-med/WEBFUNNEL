11e47eeb35c61c3ccb7718c063e3fbc7fd05deec
M-o-ha-med <120997421+M-o-ha-med@users.noreply.github.com>
2025-02-02 17:06:45 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter , Routes, Route , Router} from 'react-router-dom';
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
 import './index.css'
 import App from './App.tsx'
 import Portfolio_details from './Pages/Portfolio_details.tsx'
@@ -12,12 +12,10 @@ createRoot(document.getElementById('root')!).render(
 
   <StrictMode>
 	<BrowserRouter>
-		<Router>
 		<Routes>
 			<Route path="/" element={<App />} />
 			<Route path="/ModernTropicalHouseAceh" element={<Portfolio_details />} />
 			<Route path="/bagaimana-cara-arsitek-mendesain-suatu-bangunan" element={<ArticlePage />} />
-		</Routes>
 		</Routes>
 	</BrowserRouter>
   </StrictMode>,
