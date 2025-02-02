82a15bba1a2ae61a0bb84b37c738478820e4ce6f
M-o-ha-med <120997421+M-o-ha-med@users.noreply.github.com>
2025-02-02 17:04:20 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter , Routes, Route} from 'react-router-dom';
+import { BrowserRouter , Routes, Route , Router} from 'react-router-dom';
 import './index.css'
 import App from './App.tsx'
 import Portfolio_details from './Pages/Portfolio_details.tsx'
